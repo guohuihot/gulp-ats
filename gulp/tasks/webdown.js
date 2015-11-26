a3d9f88dcfde9b5ac4038864ce355225fefba188
guohuihot@163.com <88407500@qq.com>
2015-11-26 18:09:28 +0800

    webdown

--- gulp/tasks/webdown.js
+++ gulp/tasks/webdown.js
@@ -3,71 +3,86 @@ module.exports = function(gulp, $$, utils) {
         var request = require('request'),
             cheerio = require('cheerio'),
             iconv   = require('iconv-lite'),
-            base    = $$.jsonFile.read('./gulp/base.json'),
-            config  = base.get('webdown'),
             url     = require('url');
-        // 参数不存在返回错误信息
-        if (!$$.argv.config) {
-            cb('命令：' + base.get('help')['webdown']['content'] + '\n');
-        };
-        var args = $$.argv.config.split(',');
-        // 设置目录
-        /*if (args[2]) {
-            base.set('webdow', args[2]);
-        };*/
+        var async = require('async');
+        async.series([step1, step2, step3],
+            function(err, values) {
+                // do somethig with the err or values v1/v2/v3
+            });
 
-        var urlParse = url.parse(options.url);
-        // 下载目录
-        var webName = $$.path.join(process.cwd(), (args[2] || config.dest) + '/' + urlParse.hostname);
-        // hostname
-        var baseUrl = urlParse.protocol + '//' + urlParse.host + '/';
-        // 创建下载目录
-        // console.log(webName);
-        utils.mkdir(webName, config.src);
-        // request请求配置
-        var options = {
-            url: args[0], //要下载的网址
-            gzip: true, //是否开启gzip
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36'
-            },
-            encoding: null
-        };
-        // open
-        // require('child_process').exec('explorer ' + webName);
 
-        var requestCb = function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                config.encoding = 'utf8';
-                var arr = body.toString().match(/<meta([^>]*?)>/g);
-                if (arr) {
-                    arr.forEach(function(val) {
-                        var match = val.match(/charset\s*=\s*(.+)\"/);
-                        if (match && match[1]) {
-
-                            if (match[1].substr(0, 1) == '"') match[1] = match[1].substr(1);
-                            if (match[1].indexOf('"') > 0) match[1] = match[1].substr(0, match[1].indexOf('"'));
-
-                            config.encoding = match[1].trim();
-                            return false;
-                        }
-                    })
-                }
-                // 对数据进行转码
-                body = iconv.decode(body, config.encoding);
-                // 同时数据处理
-                acquireData(body);
-            }
-        }
-
-        request(options, requestCb);
         // processUrl
         var processUrl = function(url) {
             url = url.split('?')[0].split('/').pop();
             return url;
         }
+
+        var res = function(e, f) {
+            var base = f.data;
+            // 参数不存在返回错误信息
+            if (!$$.argv.config) {
+                cb('命令：' + base['help']['webdown']['content'] + '\n');
+            };
+            var args = $$.argv.config.split(','),
+                urlParse = url.parse(args[0]),
+                base = f.data,
+                baseConfig = base['webdown'],
+                resConfig = {
+                    url: urlParse.href, //要下载的网址
+                    gzip: true, //是否开启gzip
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36'
+                    },
+                    encoding: null
+                },
+                webConfig = {
+                    baseUrl: urlParse.protocol + '//' + urlParse.host + '/',
+                    downDir: (args[2] || baseConfig.dest) + '/' + urlParse.hostname
+                };
+            // 开始请求
+            request(resConfig, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    resConfig.encoding = 'utf8';
+                    var arr = body.toString().match(/<meta([^>]*?)>/g);
+                    if (arr) {
+                        arr.forEach(function(val) {
+                            var match = val.match(/charset\s*=\s*(.+)\"/);
+                            if (match && match[1]) {
+
+                                if (match[1].substr(0, 1) == '"') match[1] = match[1].substr(1);
+                                if (match[1].indexOf('"') > 0) match[1] = match[1].substr(0, match[1].indexOf('"'));
+
+                                resConfig.encoding = match[1].trim();
+                                return false;
+                            }
+                        })
+                    }
+                    // 对数据进行转码
+                    body = iconv.decode(body, resConfig.encoding);
+                    // 同时数据处理
+                    acquireData(body, function (data) {
+                        $$.fs.createWriteStream(webConfig.downDir + '/' + args[1])
+                    });
+                }
+            });
+            // 创建下载目录
+            utils.mkdir(webConfig.downDir, baseConfig.src);
+            // 设置目录 保存配置
+            if (args[2]) {
+                f.set({
+                    "webdown": {
+                        "dest": args[2],
+                    }
+                })
+            };
+
+            f.save();
+        }
+
+        $$.jsonFile('./gulp/base.json', res)
+
         // 处理数据
-        function acquireData(data) {
+        var acquireData = function(data, cb) {
             // console.log(data);
             var $ = cheerio.load(data, {
                 decodeEntities: false //关闭转换实体编码的功能
@@ -77,8 +92,6 @@ module.exports = function(gulp, $$, utils) {
             $('link[rel="stylesheet"]').each(function(i, el) {
                 aCss.push(processUrl($(el).attr('href')));
             });
-console.log(aCss);
-return false;
             $('script[src]').each(function(i, el) {
                 aJs.push( $(el).attr('src'));
             });
@@ -94,8 +107,7 @@ return false;
                 }
             });
             // 兼容st编辑器，多个中线注释会出错
-            var fileData = $.html().replace(/<!--/g, '<!-- ').replace(/-->/g, ' -->'),
-                filePath = webName + '/' + args[1] + '.html';
+            var fileData = $.html().replace(/<!--/g, '<!-- ').replace(/-->/g, ' -->');
             // 解析内联css
             $('style').each(function(i, el) {
                 parseCss($(el).html(), baseUrl, function(cssData1) {
@@ -103,20 +115,10 @@ return false;
                 });
             });
 
-            fileData = iconv.encode(fileData, config.encoding);
-            // 创建html文件
-            $$.fs.writeFile(filePath, fileData, function(err) {
-                if (err) {
-                    console.log('1:' + err);
-                } else {
-                    console.log(args[1] + '.html created');
-                };
-            })
-
+            cb.call(this, iconv.encode(fileData, config.encoding));
         }
-
-
-
+        // open
+        // require('child_process').exec('explorer ' + webName);
         /**
          * 下载文件
          * @param  {string}   uri      请求的url
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     debugMoudle  = ['webDown.js'],
     gulpTaskList = debugMoudle.length && debugMoudle || require('fs').readdirSync('./gulp/tasks/');
 
-$.jsonFile = require('json-file');
+$.jsonFile = require('json-file-plus');
 $.argv     = require('yargs').argv;
 $.moment   = require('moment');
 $.fs       = require('fs');
